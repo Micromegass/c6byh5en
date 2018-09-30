b38f64b88f023fca663c88e1eeba99c612a5649b
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-09-30 13:38:18 -0500

    added api methods

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+
+
+  def index
+    product = Product.all
+    render json: product
+  end
+
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product, status: 201
+    else
+      render json: { errors: product.errors }, status: 422
+    end
+  end
+
+
+ def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: product, status: 201
+ end
+
+ def update 
+    product = Product.find(params[:id])
+    if product.update_attributes(product_params)
+    render json: product, status: 201
+    else 
+      render json: { errors: product.errors }, status: 422
+    end 
+ end 
+
+
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price)
+    end
+end
